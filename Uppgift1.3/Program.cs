18c3c6d328f3a73b71f0e72573bffe224eff964b
vist07001 <vist07001@E-PW05CGR8>
2024-09-12 13:13:21 +0200

    Add project files.

---
+++ Uppgift1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Uppgift3
+{ class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /    \\");
+            Console.WriteLine("/______\\");
+        }
+    }
+}
